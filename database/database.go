74bbc203f37b9f22b28d474c9eb939dcff1aa88f
NicolasLucianoB <79168939+NicolasLucianoB@users.noreply.github.com>
2025-03-05 16:43:22 -0300

    wip

--- database/database.go
+++ database/database.go
@@ -38,27 +38,3 @@ func Connect() {
 	DB = db
 	fmt.Println("Banco de dados conectado com sucesso!")
 }
-
-// package database
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func Connect() {
-// 	dsn := os.Getenv("DB_DSN")
-// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
-// 	}
-
-// 	DB = db
-// 	fmt.Println("Banco de dados conectado com sucesso!")
-// }
